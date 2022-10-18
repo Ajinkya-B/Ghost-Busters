a365d0901ddd467b5c3e1cc57b935dbacfb4b67f
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-10-18 18:04:54 -0400

    Completed get request from MongoDB

--- back-end/api/restaurants.route.js
+++ back-end/api/restaurants.route.js
@@ -1,7 +1,18 @@
 import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+// uses the controller to call a specific api
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+    // .post(RestaurantsCtrl.apiPostRestaurants)
+    // .post(RestaurantsCtrl.apiPostRestaurant)
+
+
+// router
+//   .route("/review")
+//   .post(ReviewsCtrl.apiPostReview)
+//   .put(ReviewsCtrl.apiUpdateReview)
+//   .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -19,6 +20,7 @@ MongoClient.connect(
     process.exit(1)
   })
   .then(async client => {
+    await RestaurantsDAO.injectDB(client)
     app.listen(port, () => {
       console.log(`listening on port ${port}`)
     })

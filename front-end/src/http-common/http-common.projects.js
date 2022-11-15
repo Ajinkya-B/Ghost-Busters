1190368852a6ab1c08d29bb41abf748e31a79620
ProEdge2 <63991231+ProEdge2@users.noreply.github.com>
2022-11-14 22:32:19 -0500

    Changed the links to the newly deployed backend

--- front-end/src/http-common/http-common.projects.js
+++ front-end/src/http-common/http-common.projects.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://ghost-busters-f6c6b7uoga-uc.a.run.app/api/v1/projects",
+    baseURL: "https://ghost-busters-backend-f6c6b7uoga-uc.a.run.app/api/v1/projects",
     // headers: {
     //     "Content-type": "application/json"
     // }

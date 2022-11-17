98971830291e2e0b9ed98339920ceab0c0a365a3
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-17 10:57:19 -0500

    Welcome page and package.json updates

--- front-end/src/pages/DashboardWelcome.js
+++ front-end/src/pages/DashboardWelcome.js
@@ -3,8 +3,13 @@ import React, {Component} from "react";
 export default class DashboardWelcome extends Component {
     render() {
         return (
-            <div>
-                Hello
+            <div className='container-fluid'>
+                <h1 className='h1 title'>
+                    Welcome to your Ghostboard!
+                </h1>
+                <h4 className="subtitle">
+                    Please select a project.
+                </h4>
             </div>
         )
     }

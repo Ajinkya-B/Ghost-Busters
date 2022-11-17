34e1efcae9bdbab1b71c912c6a82b55e9b5ac0f4
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-11-17 08:21:40 -0500

    Delete back-end/useCaseInteractors directory
    Accidental commit!

--- back-end/useCaseInteractors/UploadTranscript.js
+++
@@ -1,5 +0,0 @@
-/**
- * Use Case Interactor for the Upload Transcript use case
- * This interactor should get data from the selected project api and pass the cleaned up data to the DAO
- * to be inputted into the database and should return success back to the view
- */

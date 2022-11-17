86b688d4adc6d855d509d70cd4ae34c5cd0ffdaa
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-16 21:38:56 -0500

    partially merged development branch into this branch

--- back-end/api/controllers/projects.controller.js
+++ back-end/api/controllers/projects.controller.js
@@ -5,6 +5,57 @@ import {project} from "../../entities/project.js";
 export default class ProjectsController {
 
     /**
+     * A GET API for getting an array of all project objects from MongoDB.
+     */
+    static async apiGetAllProjects(req, res, next) {
+        // Formatting as a list of projects
+        // let itemsSoFar = []
+        // let response = await ProjectsDAO.getAllProjects()
+        // for (let x = 0; x < response.length; x++) {
+        //     let tempObject = new project(response[x]["project_name"], response[x]["project_id"],
+        //         response[x]["api_key"], response[x]["transcripts"]);
+        //     itemsSoFar.push(tempObject);
+        // }
+        // console.log(itemsSoFar);
+
+        let filters = {}
+        if (req.query.project_name) {
+            filters.project_name = req.query.project_name
+        } else if (req.query.project_id) {
+            filters.project_id = req.query.project_id
+        }
+
+        const projectsList = await ProjectsDAO.getAllProjects({filters})
+
+        // let response = {
+        //     projects: projectsList,
+        //     filters: filters
+        // }
+
+        let response = projectsList
+
+        res.json(response);
+    }
+
+    /**
+     * A GET API for getting a project object with a particular id from MongoDB.
+     */
+    static async apiGetProjectByID(req, res, next) {
+        try {
+            let id = req.params.id || {}
+            let project = await ProjectsDAO.getProjectByID(id)
+            if (!project) {
+                res.status(404).json({ error: "Not found" })
+                return
+            }
+            res.json(project)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+    /**
      * A POST API for creating a project object in MongoDB.
      * @param req
      * @param res
@@ -35,36 +86,32 @@ export default class ProjectsController {
         }
     }
 
-    /**
-     * A GET API for getting an array of all project objects from MongoDB.
-     */
-    static async apiGetAllProjects(req, res, next) {
-        // let itemsSoFar = []
-        // let response = await ProjectsDAO.getAllProjects()
-        // for (let x = 0; x < response.length; x++) {
-        //     let tempObject = new project(response[x]["project_name"], response[x]["project_id"],
-        //         response[x]["api_key"], response[x]["transcripts"]);
-        //     itemsSoFar.push(tempObject);
-        // }
-        // console.log(itemsSoFar);
+    static async apiUpdateProject(req, res, next) {
+        try {
+            const projectName = req.body.project_name
+            const transcript = req.body.transcripts
 
-        let filters = {}
-        if (req.query.project_name) {
-            filters.project_name = req.query.project_name
-        } else if (req.query.project_id) {
-            filters.project_id = req.query.project_id
+            const projectResponse = await ProjectsDAO.updateProject(
+                projectName,
+                // req.body.project_name,
+                transcript
+            )
+
+            const {error} = projectResponse;
+            if (error) {
+                res.status(400).json({ error })
+            }
+
+            if (projectResponse.modifiedCount === 0) {
+                throw new Error(
+                    "unable to update project - project selection may be incorrect",
+                )
+            }
+
+            res.json({ status: "success" })
+        } catch (e) {
+            res.status(500).json({ error: e.message })
         }
-
-        const projectsList = await ProjectsDAO.getAllProjects({filters})
-
-        // let response = {
-        //     projects: projectsList,
-        //     filters: filters
-        // }
-
-        let response = projectsList
-
-        res.json(response);
-
     }
+
 }
--- back-end/dao/projectsDAO.js
+++ back-end/dao/projectsDAO.js
@@ -16,25 +16,6 @@ export default class ProjectsDAO {
     }
 
     /**
-     * Create a project object in MongoDB (see AddProject.js in /front-end for more info).
-     */
-    static async createProject(req, res, next){
-        await projects.insertOne(req)
-    }
-
-    static async deleteProject(projectName) {
-        try {
-            const deleteResponse = await projects.deleteOne({
-                project_name: projectName
-            })
-            return deleteResponse
-        } catch (e) {
-            console.error(`Unable to delete project: ${e}`)
-            return { error: e }
-        }
-    }
-
-    /**
      * Get an array of all the projects (with or without filters) from MongoDB.
      * @returns an array of all the projects from the database
      */
@@ -56,8 +37,85 @@ export default class ProjectsDAO {
             console.error(`Unable to issue find command, ${e}`)
             return []
         }
-
+        // The previous version of getAllProjects just had this return statement:
         // return await projects.find().toArray();
+    }
+
+    /**
+     * Get a project object with a particular id from MongoDB.
+     * @param id
+     */
+    static async getProjectByID(id) {
+        try {
+            const pipeline = [
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "text_transcripts",
+                        let: {
+                            id: "$_id",
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$project_id", "$$id"],
+                                    },
+                                },
+                            }
+
+                        ],
+                        as: "transcripts",
+                    },
+                },
+                {
+                    $addFields: {
+                        transcripts: "$transcripts",
+                    },
+                },
+            ]
+            return await projects.aggregate(pipeline).next()
+        } catch (e) {
+            console.error(`Something went wrong in getProjectByID: ${e}`)
+            throw e
+        }
+    }
+
+    /**
+     * Create a project object in MongoDB (see AddProject.js in /front-end for more info).
+     */
+    static async createProject(req, res, next){
+        await projects.insertOne(req)
+    }
+
+    static async deleteProject(projectName) {
+        try {
+            const deleteResponse = await projects.deleteOne({
+                project_name: projectName
+            })
+            return deleteResponse
+        } catch (e) {
+            console.error(`Unable to delete project: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async updateProject(projectName, transcript) {
+        try {
+            const updateResponse = await projects.updateOne(
+                { project_name: projectName},
+                { $addToSet: { transcripts: transcript } },
+            )
+
+            return updateResponse
+        } catch (e) {
+            console.error(`Unable to update project: ${e}`)
+            return { error: e }
+        }
     }
 
 }

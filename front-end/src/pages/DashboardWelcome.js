1038cf090ffcabbbf07efaca16178abbedc6548c
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-18 21:19:24 -0500

    created a welcome page (but it's not visible yet)

--- front-end/src/pages/DashboardWelcome.js
+++ front-end/src/pages/DashboardWelcome.js
@@ -1,16 +1,27 @@
-import React, {Component} from "react";
+import React from "react";
+import {NavBtn, NavBtnLink} from "../components/NavbarElements";
 
-export default class DashboardWelcome extends Component {
-    render() {
-        return (
-            <div className='container-fluid'>
-                <h1 className='h1 title'>
-                    Welcome to your Ghostboard!
-                </h1>
-                <h4 className="subtitle">
-                    Please select a project.
-                </h4>
-            </div>
-        )
+
+export default function DashboardWelcomePage() {
+    const style = {
+        margin: "auto",
+        padding: "10% 35% 10% 15%",
+        color: "white"
     }
+    return (
+        <div className='container-fluid' style={style}>
+            <h1 className='h1 title'>
+                Welcome to your Ghostboard!
+            </h1>
+            <h4 className="subtitle">
+                Please select a project.
+            </h4>
+            <NavBtn>
+                <NavBtnLink to={'/ManageProject'}>
+                    Click me or "Manage Projects"!
+                </NavBtnLink>
+            </NavBtn>
+        </div>
+    )
+
 }

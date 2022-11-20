fe0c49f133a82b89c455421601f55ae4b3b515b3
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-19 22:49:48 -0500

    writing tests

--- front-end/src/__test__/DashboardWelcome.test.js
+++ front-end/src/__test__/DashboardWelcome.test.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import DashboardWelcomePage from "../pages/DashboardWelcome.js";
-//
-//
-// describe("DashboardWelcome Component", function () {
-//
-//     it("should have welcome message", function () {
-//         let { getByText } = render(<DashboardWelcomePage />);
-//         expect(getByText("Welcome to your Ghostboard!")).toMatchInlineSnapshot(`
-//             <h1>
-//             Welcome to your Ghostboard!
-//           </h1>
-//     `);
-//     });
-// });
+import React from 'react';
+import { render } from '@testing-library/react';
+import DashboardWelcomePage from "../pages/DashboardWelcome.js";
+
+
+describe("DashboardWelcome Component", function () {
+
+    it("should have welcome message", function () {
+        let { getByText } = render(<DashboardWelcomePage />);
+        expect(getByText("Welcome to your Ghostboard!")).toMatchInlineSnapshot(`
+            <h1>
+            Welcome to your Ghostboard!
+          </h1>
+    `);
+    });
+});

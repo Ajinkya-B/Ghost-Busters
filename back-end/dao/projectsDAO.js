796f1a51dd4c07fd9b499a71f0d444293cb72475
khushiagl <khushi.agrawal@mail.utoronto.ca>
2022-11-21 17:16:57 -0500

    bug fix

--- back-end/dao/projectsDAO.js
+++ back-end/dao/projectsDAO.js
@@ -1,31 +1,31 @@
-
-import { ObjectId } from "mongodb";
-import { Projects } from "../schema/projects-schema.js"
+import {ObjectId} from "mongodb";
+import {Projects} from "../schema/projects-schema.js"
 import mongoose from "mongoose";
-
 
 
 export default class ProjectsDAO {
 
 
-  static async getProjects(query) {
-    try {
-      return await Projects.find(query).exec();
-    } catch (e) {
-      console.error(`Unable to issue find command, ${e}`);
-      return [];
+    static async getProjects(query) {
+        try {
+            return await Projects.find(query).exec();
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`);
+            return [];
+        }
     }
 
 
-  /**
-   * Create a project object in MongoDB (see AddProject.js in /front-end for more info).
-   */
-  static async createProject(body) {
-    try{
-      await Projects.create(body);
-    }catch(e){
-        console.error(`Unable to issue create command, ${e}`);
+    /**
+     * Create a project object in MongoDB (see AddProject.js in /front-end for more info).
+     */
+    static async createProject(body) {
+        try {
+            await Projects.create(body);
+        } catch (e) {
+            console.error(`Unable to issue create command, ${e}`);
 
+        }
     }
 
 
@@ -34,15 +34,16 @@ export default class ProjectsDAO {
      * @param projectName
      * @returns {Promise<{error}|*>}
      */
-  static async deleteProject(projectName) {
-    try {
-      const deleteResponse = await Projects.deleteOne({
-        project_name: projectName,
-      });
-      return deleteResponse;
-    } catch (e) {
-      console.error(`Unable to delete project: ${e}`);
-      return { error: e };
+    static async deleteProject(projectName) {
+        try {
+            const deleteResponse = await Projects.deleteOne({
+                project_name: projectName,
+            });
+            return deleteResponse;
+        } catch (e) {
+            console.error(`Unable to delete project: ${e}`);
+            return {error: e};
+        }
     }
 
 
@@ -52,7 +53,8 @@ export default class ProjectsDAO {
      * @param transcript
      * @returns {Promise<{error}|*>}
      */
-    static async updateProject(projectName, transcript) {
+    static
+    async updateProject(projectName, transcript) {
         try {
             const updateResponse = await projects.updateOne(
                 {project_name: projectName},
@@ -73,63 +75,65 @@ export default class ProjectsDAO {
      * @param id
      * @returns {Promise<*>}
      */
-  static async getProjectByID(id) {
-    try {
-      const pipeline = [
-        {
-          $match: {
-            _id: new mongoose.Types.ObjectId(id) ,
-          },
-        },
-        {
-          $lookup: {
-            from: "text transcripts",
-            let: {
-              id: "$project_id",
-            },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $eq: ["$project_id", "$$id"],
-                  },
+    static
+    async getProjectByID(id) {
+        try {
+            const pipeline = [
+                {
+                    $match: {
+                        _id: new mongoose.Types.ObjectId(id),
+                    },
                 },
-              },
-            ],
-            as: "text_transcripts",
-          },
-        },
-        {
-          $lookup: {
-            from: "transcripts",
-            let: {
-              id: "$project_id",
-            },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $eq: ["$project_id", "$$id"],
-                  },
+                {
+                    $lookup: {
+                        from: "text transcripts",
+                        let: {
+                            id: "$project_id",
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$project_id", "$$id"],
+                                    },
+                                },
+                            },
+                        ],
+                        as: "text_transcripts",
+                    },
                 },
-              },
-            ],
-            as: "transcripts",
-          },
-        },
-        {
-          $addFields: {
-            transcripts: "$transcripts",
-            text_transcripts: "$text_transcripts"
-          },
-        },
-      ];
-      const temp = await Projects.aggregate(pipeline);
-      return temp[0];
-      // return await Projects.aggregate(pipeline)
-    } catch (e) {
-      console.error(`Something went wrong in getProjectByID: ${e}`);
-      throw e;
+                {
+                    $lookup: {
+                        from: "transcripts",
+                        let: {
+                            id: "$project_id",
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$project_id", "$$id"],
+                                    },
+                                },
+                            },
+                        ],
+                        as: "transcripts",
+                    },
+                },
+                {
+                    $addFields: {
+                        transcripts: "$transcripts",
+                        text_transcripts: "$text_transcripts"
+                    },
+                },
+            ];
+            const temp = await Projects.aggregate(pipeline);
+            return temp[0];
+            // return await Projects.aggregate(pipeline)
+        } catch (e) {
+            console.error(`Something went wrong in getProjectByID: ${e}`);
+            throw e;
+        }
     }
 }
 
--- back-end/helpers/transcriptDataFormatter.js
+++ back-end/helpers/transcriptDataFormatter.js
@@ -1,13 +1,13 @@
 export default class transcriptDataFormatter {
 
-  /**
-   * Cleans up and formats raw transcript data
-   * @param {Array} transcript
-   * @returns
-   */
-  static cleanData(transcript) {
-    var res = [];
-    var block;
+    /**
+     * Cleans up and formats raw transcript data
+     * @param {Array} transcript
+     * @returns
+     */
+    static cleanData(transcript) {
+        var res = [];
+        var block;
 
         transcript.forEach(function (node) {
             block = {
@@ -21,51 +21,54 @@ export default class transcriptDataFormatter {
         return res;
     }
 
-  // This function helps the use case format the data from the api call, it returns the text data
-  // from each individual transcript
-  static cleanTextTranscript(transcipt) {
-    try {
-      let res = [];
-      let tempBotChat;
-      let question;
+    // This function helps the use case format the data from the api call, it returns the text data
+    // from each individual transcript
+    static cleanTextTranscript(transcipt) {
+        try {
+            let res = [];
+            let tempBotChat;
+            let question;
 
-      // this checks if the data contains either text or an intent from the user, it captures it in the proper format
-      // and returns it to the service layer in the form of an array
-      transcipt.forEach(function (step) {
-        let data;
-        if (step.payload && (step.payload.type === "text" || step.payload.type === "intent")) {
-          if (step.payload.payload && step.payload.payload.message) {
-              question = String(step.payload.payload.message);
-              // Checks if current block is said my a bot
-              if (!question.includes("audio")) {
-                tempBotChat = question
-                //     question.substring(
-                //   question.indexOf(">") + 1,
-                //   question.lastIndexOf("<")
-                // );
-                if (tempBotChat !== "") {
-                  data = {
-                    speaker: "bot",
-                    text: tempBotChat,
-                  };
-                  res.push(data);
+            // this checks if the data contains either text or an intent from the user, it captures it in the proper format
+            // and returns it to the service layer in the form of an array
+            transcipt.forEach(function (step) {
+                let data;
+                if (step.payload && (step.payload.type === "text" || step.payload.type === "intent")) {
+                    if (step.payload.payload && step.payload.payload.message) {
+                        question = String(step.payload.payload.message);
+                        // Checks if current block is said my a bot
+                        if (!question.includes("audio")) {
+                            tempBotChat = question
+                            //     question.substring(
+                            //   question.indexOf(">") + 1,
+                            //   question.lastIndexOf("<")
+                            // );
+                            if (tempBotChat !== "") {
+                                data = {
+                                    speaker: "bot",
+                                    text: tempBotChat,
+                                };
+                                res.push(data);
+                            }
+                        }
+                    }
+                    if (step.payload.payload.query) {
+                        let answer = String(step.payload.payload.query);
+                        data = {
+                            speaker: "human",
+                            text: answer,
+                        };
+                        res.push(data);
+                    }
                 }
-              }
-            }
-            if (step.payload.payload.query) {
-              let answer = String(step.payload.payload.query);
-              data = {
-                speaker: "human",
-                text: answer,
-              };
-              res.push(data);
-            }
-          }
-        // }
-      });
-      return res;
-    } catch (e) {
-      throw e;
+                // }
+            });
+            return res;
+        } catch (e) {
+            throw e;
+
+        }
 
     }
+
 }

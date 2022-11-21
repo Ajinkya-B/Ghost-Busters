848646651f841d9fc4f8449d881ad7dd8e2f3c79
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-21 16:24:07 -0500

    writing tests

--- front-end/src/__test__/DashboardWelcome.test.js
+++ front-end/src/__test__/DashboardWelcome.test.js
@@ -6,10 +6,12 @@ import React from 'react';
 import "@testing-library/jest-dom";
 import {cleanup, render} from '@testing-library/react';
 import DashboardWelcomePage from "../pages/DashboardWelcome.js";
+import {act} from "react-test-renderer";
 
 it("navigates to Manage Projects page when you click button", () => {
     const root = document.createElement('div');
     document.body.appendChild(root);
-    // render
-})
+ 
+});
+
 

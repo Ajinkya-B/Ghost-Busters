38e9521836cd26b105ca2b3118917e1d81a119ae
khushiagl <khushi.agrawal@mail.utoronto.ca>
2022-11-20 20:40:20 -0500

    connected analyse project use case to front-end temporarily. Still need to discuss and implement finalized design

--- front-end/src/components/Project.js
+++ front-end/src/components/Project.js
@@ -93,43 +93,8 @@ const Project = props => {
                     {/*<Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-primary">*/}
                     {/*    Add Review*/}
                     {/*</Link>*/}
-                    <h4> Analysed Transcripts </h4>
-                    <div className="row">
-                        {project.transcripts.length > 0 ? (
-                            project.transcripts.map((transcript, index) => {
-                                return (
-                                    <div className="col-lg-4 pb-1" key={index}>
-                                        <div className="card">
-                                            <div className="card-body">
-                                                <p className="card-text">
-                                                    {transcript._id}<br/>
-                                                    <strong>Duration: </strong>{"duration"}<br/>
-                                                    <strong>Bot's last Text: </strong>{"bot"}<br/>
-                                                    <strong>Human's last Text: </strong>{"human"}
-                                                </p>
-                                                {/*{props.user && props.user.id === review.user_id &&*/}
-                                                {/*    <div className="row">*/}
-                                                {/*        <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>*/}
-                                                {/*        <Link to={{*/}
-                                                {/*            pathname: "/restaurants/" + props.match.params.id + "/review",*/}
-                                                {/*            state: {*/}
-                                                {/*                currentReview: review*/}
-                                                {/*            }*/}
-                                                {/*        }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>*/}
-                                                {/*    </div>*/}
-                                                {/*}*/}
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        ) : (
-                            <div className="col-sm-4">
-                                <p>No transcripts uploaded.</p>
-                            </div>
-                        )}
 
-                    </div>
+
 
                 </div>
             ) : (
--- front-end/src/http-common/http-common.analyseProject.js
+++ front-end/src/http-common/http-common.analyseProject.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://ghost-busters-backend-f6c6b7uoga-uc.a.run.app/api/v1/analyseProject",
+    baseURL: "http://localhost:8000/api/v1/analyseProject",
 
 });

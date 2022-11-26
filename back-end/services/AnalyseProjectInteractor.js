03850d8a29e4747d0dcf16786486fb8aa25dbd3d
khushiagl <khushi.agrawal@mail.utoronto.ca>
2022-11-25 21:40:26 -0500

    changes implementation of totalusersforcequit to return #users quit per day

--- back-end/services/AnalyseProjectInteractor.js
+++ back-end/services/AnalyseProjectInteractor.js
@@ -1,134 +1,172 @@
-import {getDurationTime, getDurationTexts, userForceQuit, checkReason} from './AnalyseTranscriptInteractor.js'
+import AnalyseTranscriptInteractor from './AnalyseTranscriptInteractor.js'
 
-/**
- * Returns the average length of conversations of the transcripts.
- * @param text_transcripts
- * @returns {number}
- */
-export function avgDurationTexts(text_transcripts) {
-    if (text_transcripts.length > 0) {
+class AnalyseProjectInteractor {
+
+    /**
+     * Returns the average length of conversations of the transcripts.
+     * @param text_transcripts
+     * @returns {number}
+     */
+    avgDurationTexts(text_transcripts) {
+        if (text_transcripts.length > 0) {
+            let total_duration = 0;
+            let l = text_transcripts.length;
+            for (let j = 0; j < l; j++) {
+                total_duration += AnalyseTranscriptInteractor.getDurationTexts(
+                    text_transcripts[j].dialogue);
+            }
+            return total_duration / l;
+        }
+
+        return 0;
+
+    }
+
+    /**
+     * Return the average duration of conversations of the transcripts.
+     * @param transcripts
+     * @returns {number}
+     */
+    avgDurationTime(transcripts) {
         let total_duration = 0;
-        let l = text_transcripts.length;
+        let l = transcripts.length;
         for (let j = 0; j < l; j++) {
-            total_duration += getDurationTexts(text_transcripts[j].dialogue);
+            let transcript = transcripts[j];
+            total_duration += AnalyseTranscriptInteractor.getDurationTime(transcript.transcript_data);
         }
-        return total_duration / l;
+        return total_duration / transcripts.length;
     }
 
-    return 0;
+    /**
+     * Returns the third quantile ( 75 percentile) of the durations of the conversations.
+     * @param transcripts
+     * @returns {*}
+     */
+    thirdQuantileTime(transcripts) {
+        let l = transcripts.length;
+        let Q3 = Math.floor(3 * (l + 1) / 4);
+        let durations = [];
+        for (let j = 0; j < l; j++) {
+            durations.push(AnalyseTranscriptInteractor.getDurationTime(transcripts[j].transcript_data));
+        }
+        durations.sort(function (a, b) {
+            return a - b
+        });
+        console.log(durations);
+        return durations[Q3 - 1];
 
+    }
+
+    /**
+     * Returns the third quantile ( 75 percentile) of the length of the conversations.
+     * @param text_transcripts
+     * @returns {*}
+     */
+    thirdQuantileTexts(text_transcripts) {
+        let l = text_transcripts.length;
+        let Q3 = Math.floor(3 * (l + 1) / 4);
+        let durations = [];
+        for (let j = 0; j < l; j++) {
+            durations.push(AnalyseTranscriptInteractor.getDurationTexts(text_transcripts[j].dialogue));
+        }
+        durations.sort(function (a, b) {
+            return a - b
+        });
+        console.log(durations);
+        return durations[Q3 - 1];
+
+    }
+
+    totalConvosPerDay(transcripts) {
+        let l = transcripts.length;
+        let map = new Object();
+
+        for (let j = 0; j < l; j++) {
+            let transcript = transcripts[j];
+            let date = new Date(transcript.transcript_data[l - 1].startTime.toString().slice(0, 10));
+            let key = date.getDate().toString() + date.getMonth().toString();
+            if (map[key]) {
+                map[key] += 1;
+            } else {
+                map[key] = 1
+            }
+
+
+        }
+        return map;
+    }
+
+
+    /**
+     * Returns the total numbers of users that force quit chats or weren't provided with satisfatory solutions.
+     * @param text_transcripts
+     * @returns {number}
+     */
+    totalUsersForceQuit(text_transcripts, transcripts) {
+        let l = text_transcripts.length;
+        let map = new Object();
+
+        for (let j = 0; j < l; j++) {
+            let textTranscript = text_transcripts[j];
+            let transcript = transcripts[j];
+            let date = new Date(transcript.transcript_data[l - 1].startTime.toString().slice(0, 10));
+            let key = date.getDate().toString() + date.getMonth().toString();
+            if (AnalyseTranscriptInteractor.userForceQuit(textTranscript.dialogue)) {
+                if (map[key]) {
+                    map[key] += 1;
+                } else {
+                    map[key] = 1
+                }
+                console.log(map)
+
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Return the no.of users that quit corresponding to each reason.
+     * @param text_transcripts
+     * @returns {{other: number, human_interaction: number, no_solution: number, privacy: number}}
+     */
+    checkReasons(text_transcripts, transcripts) {
+        let l_text = text_transcripts.length;
+        let Q3_text = this.thirdQuantileTexts(text_transcripts);
+        let Q3_time = this.thirdQuantileTime(transcripts);
+        let reasons = {
+            "privacy": 0,
+            "no_solution": 0,
+            "human_interaction": 0,
+            "lengthy_convo": 0,
+            "other": 0
+        };
+        for (let j = 0; j < l_text; j++) {
+
+            let textTranscript = text_transcripts[j];
+            let transcript = transcripts[j];
+
+            let temp = AnalyseTranscriptInteractor.checkReason(
+                textTranscript, Q3_text, transcript, Q3_time);
+            if (temp.includes("privacy")) {
+                reasons.privacy += 1;
+            }
+            if (temp.includes("nosolution")) {
+                reasons.no_solution += 1;
+            }
+            if (temp.includes("humaninteraction")) {
+                reasons.human_interaction += 1;
+            }
+            if (temp.includes("lengthyConvo")) {
+                reasons.lengthy_convo += 1;
+            }
+            if (temp.includes("other")) {
+                reasons.other += 1;
+            }
+        }
+        return reasons;
+    }
 }
 
-/**
- * Return the average duration of conversations of the transcripts.
- * @param transcripts
- * @returns {number}
- */
-export function avgDurationTime(transcripts) {
-    let total_duration = 0;
-    let l = transcripts.length;
-    for (let j = 0; j < l; j++) {
-        let transcript = transcripts[j];
-        total_duration += getDurationTime(transcript.transcript_data);
-    }
-    return total_duration / transcripts.length;
-}
-
-/**
- * Returns the third quantile ( 75 percentile) of the durations of the conversations.
- * @param transcripts
- * @returns {*}
- */
-export function thirdQuantileTime(transcripts){
-    let l = transcripts.length;
-    let Q3 = Math.floor(3*(l + 1)/4);
-    let durations = [];
-    for (let j = 0; j < l; j++) {
-        durations.push(getDurationTime(transcripts[j].transcript_data));
-    }
-    durations.sort(function (a, b){return a-b});
-    console.log(durations);
-    return durations[Q3 - 1];
-
-}
-
-/**
- * Returns the third quantile ( 75 percentile) of the length of the conversations.
- * @param text_transcripts
- * @returns {*}
- */
-export function thirdQuantileTexts(text_transcripts){
-    let l = text_transcripts.length;
-    let Q3 = Math.floor(3*(l + 1)/4);
-    let durations = [];
-    for (let j = 0; j < l; j++) {
-        durations.push(getDurationTexts(text_transcripts[j].dialogue));
-    }
-    durations.sort(function (a, b){return a-b});
-    console.log(durations);
-    return durations[Q3 - 1];
-
-}
-
-
-/**
- * Returns the total numbers of users that force quit chats or weren't provided with satisfatory solutions.
- * @param text_transcripts
- * @returns {number}
- */
-export function totalUsersForceQuit(text_transcripts) {
-    let i = 0;
-    let l = text_transcripts.length;
-    for (let j = 0; j < l; j++) {
-
-        let transcript = text_transcripts[j];
-        if (userForceQuit(transcript.dialogue)) {
-            i = i + 1;
-            console.log(i);
-        }
-    }
-    return i;
-}
-
-/**
- * Return the no.of users that quit corresponding to each reason.
- * @param text_transcripts
- * @returns {{other: number, human_interaction: number, no_solution: number, privacy: number}}
- */
-export function checkReasons(text_transcripts, transcripts) {
-    let l_text = text_transcripts.length;
-    let Q3_text = thirdQuantileTexts(text_transcripts);
-    let Q3_time = thirdQuantileTime(transcripts);
-    let reasons = {
-        "privacy": 0,
-        "no_solution": 0,
-        "human_interaction": 0,
-        "lengthy_convo": 0,
-        "other": 0
-    }
-    for (let j = 0; j < l_text; j++) {
-
-        let textTranscript = text_transcripts[j];
-        let transcript = transcripts[j];
-
-        let temp = checkReason(textTranscript, Q3_text, transcript, Q3_time);
-        if (temp.includes("privacy")) {
-            reasons.privacy += 1;
-        }
-        if (temp.includes("nosolution")) {
-            reasons.no_solution += 1;
-        }
-        if ( temp.includes("humaninteraction")){
-            reasons.human_interaction += 1;
-        }
-        if (temp.includes("lengthyConvo")){
-            reasons.lengthy_convo += 1;
-        }
-        if ( temp.includes("other")){
-            reasons.other += 1;
-        }
-    }
-    return reasons;
-}
+export default new AnalyseProjectInteractor();
 
 
--- back-end/services/AnalyseTranscriptInteractor.js
+++ back-end/services/AnalyseTranscriptInteractor.js
@@ -1,147 +1,153 @@
 import {isPrivacyConcern, isNoSolution, isHumanInteraction, isLengthyConvo} from "./checkReasonsInterctor.js"
 
-/**
- * Returns whether the user force quit a chat or wasn't satisfied.
- * @param text_transcript
- * @returns {boolean}
- */
-export function userForceQuit(text_transcript) {
-    const keywords = ["thank you", "next time", "goodbye", "see you soon", "good day", "exit"]
-    let l = text_transcript.length;
-    let text = text_transcript[l - 1].text.toLowerCase();
-    console.log(text);
-    if (keywords.some(keyword => text.includes(keyword))) {
+
+class AnalyseTranscriptInteractor {
+    /**
+     * Returns whether the user force quit a chat or wasn't satisfied.
+     * @param text_transcript
+     * @returns {boolean}
+     */
+    userForceQuit(text_transcript) {
+        const keywords = ["thank you", "next time", "goodbye", "see you soon", "good day", "exit"]
+        let l = text_transcript.length;
+        let text = text_transcript[l - 1].text.toLowerCase();
         console.log(text);
-        return false;
+        if (keywords.some(keyword => text.includes(keyword))) {
+            console.log(text);
+            return false;
+        }
+        return true;
+
+        // let l = transcript.length;
+        // try {
+        //     if (transcript[l - 1].payload.type === "exit") {
+        //         return false;
+        //     }
+        //     return true;
+        // } catch (e) {
+        //     return true;
+        //
+        // }
     }
-    return true;
 
-    // let l = transcript.length;
-    // try {
-    //     if (transcript[l - 1].payload.type === "exit") {
-    //         return false;
-    //     }
-    //     return true;
-    // } catch (e) {
-    //     return true;
-    //
-    // }
-}
+    /**
+     * Returns the last message of the conversation along with the speaker of that message.
+     * @param text_transcript
+     * @returns {*[]}
+     */
+    getLastText(text_transcript) {
+        // let transcript = [];
+        // transcript = await databaseFunctions.getTrim();
 
-/**
- * Returns the last message of the conversation along with the speaker of that message.
- * @param text_transcript
- * @returns {*[]}
- */
-export function getLastText(text_transcript) {
-    // let transcript = [];
-    // transcript = await databaseFunctions.getTrim();
-
-    let l = text_transcript.dialouge.length;
-    console.log([text_transcript.dialouge[l - 1].text, text_transcript.dialouge[l - 1].speaker]);
-    return [text_transcript.dialouge[l - 1].text, transcript.dialouge[l - 1].speaker];
-}
-
-/**
- * Returns the last text spoken by the bot.
- * @param text_transcript
- * @returns {string|*}
- */
-export function getLastTextBot(text_transcript) {
-    // let transcript = [];
-    // transcript = await databaseFunctions.getTrim();
-
-    let l = text_transcript.dialouge.length;
-    for (let x = l - 1; x >= 0; x--) {
-        if (text_transcript.dialouge[x].speaker == "bot") {
-            console.log([text_transcript.dialouge[x].text, text_transcript.dialouge[x].speaker]);
-            return text_transcript.dialouge[x];
-        }
+        let l = text_transcript.dialouge.length;
+        console.log([text_transcript.dialouge[l - 1].text, text_transcript.dialouge[l - 1].speaker]);
+        return [text_transcript.dialouge[l - 1].text, transcript.dialouge[l - 1].speaker];
     }
-    console.log("Bot doesn't speak during the conversation")
-    return "Bot doesn't speak during the conversation"
-}
 
-/**
- * Returns the last text spoken by the human.
- * @param text_transcript
- * @returns {string|*}
- */
-export function getLastTextHuman(text_transcript) {
-    // let transcript = [];
-    // transcript = await databaseFunctions.getTrim();
-    let l = text_transcript.dialouge.length;
-    for (let x = l - 1; x >= 0; x--) {
-        if (text_transcript.dialogue[x].speaker == "human") {
-            console.log([text_transcript.dialogue[x].text, text_transcript.dialogue[x].speaker]);
-            return text_transcript.dialogue[x];
-        }
-    }
-    console.log("Human doesn't speak during the conversation")
-    return "Human doesn't speak during the conversation"
-}
+    /**
+     * Returns the last text spoken by the bot.
+     * @param text_transcript
+     * @returns {string|*}
+     */
+     getLastTextBot(text_transcript) {
+        // let transcript = [];
+        // transcript = await databaseFunctions.getTrim();
 
-/**
- * Returns the length of the conversation between bot and human in no.of texts exchanged.
- * @param text_transcript
- * @returns {*}
- */
-export function getDurationTexts(text_transcript) {
-    // let transcript =[];
-    // transcript = await databaseFunctions.getTrim();
-
-    let l = text_transcript.length;
-    return l;
-}
-
-/**
- * Returns the duration of the conversation between bot and human in milliseconds.
- * @param transcript
- * @returns {number}
- */
-export function getDurationTime(transcript) {
-    // let transcript = [];
-    // transcript = await databaseFunctions.getTrim();
-
-    const l = transcript.length;
-
-    let end_time = Date.parse(transcript[l - 1].startTime);
-    let start_time = Date.parse(transcript[0].startTime);
-    // console.log(start_time);
-    // console.log(Math.abs(end_time - start_time));
-    return Math.abs(end_time - start_time);
-}
-
-
-/**
- * Return the possible reasons why user left the chat.
- * @param text_transcript
- * @returns {*[]}
- */
-export function checkReason(text_transcript, Q3_text, transcript, Q3_time){
-    let dialogues = text_transcript.dialogue;
-    let l_texts = getDurationTexts(dialogues);
-    let l_time = getDurationTime(transcript.transcript_data);
-    let reasons = [];
-    if (userForceQuit(dialogues)){
-        if (isPrivacyConcern(dialogues)){
-            reasons.push("privacy");
-        }
-        if (isNoSolution(dialogues)){
-            reasons.push("nosolution");
-        }
-        if (isHumanInteraction(dialogues)){
-            reasons.push("humaninteraction");
+        let l = text_transcript.dialouge.length;
+        for (let x = l - 1; x >= 0; x--) {
+            if (text_transcript.dialouge[x].speaker == "bot") {
+                console.log([text_transcript.dialouge[x].text, text_transcript.dialouge[x].speaker]);
+                return text_transcript.dialouge[x];
             }
-        if (isLengthyConvo(l_texts, Q3_text, l_time, Q3_time)){
-            reasons.push("lengthyConvo")
         }
-        if (reasons.length == 0){
-            reasons.push("other");
-        }
-
+        console.log("Bot doesn't speak during the conversation")
+        return "Bot doesn't speak during the conversation"
     }
-    return reasons;
+
+    /**
+     * Returns the last text spoken by the human.
+     * @param text_transcript
+     * @returns {string|*}
+     */
+    getLastTextHuman(text_transcript) {
+        // let transcript = [];
+        // transcript = await databaseFunctions.getTrim();
+        let l = text_transcript.dialouge.length;
+        for (let x = l - 1; x >= 0; x--) {
+            if (text_transcript.dialogue[x].speaker == "human") {
+                console.log([text_transcript.dialogue[x].text, text_transcript.dialogue[x].speaker]);
+                return text_transcript.dialogue[x];
+            }
+        }
+        console.log("Human doesn't speak during the conversation")
+        return "Human doesn't speak during the conversation"
+    }
+
+    /**
+     * Returns the length of the conversation between bot and human in no.of texts exchanged.
+     * @param text_transcript
+     * @returns {*}
+     */
+    getDurationTexts(text_transcript) {
+        // let transcript =[];
+        // transcript = await databaseFunctions.getTrim();
+
+        let l = text_transcript.length;
+        return l;
+    }
+
+    /**
+     * Returns the duration of the conversation between bot and human in milliseconds.
+     * @param transcript
+     * @returns {number}
+     */
+    getDurationTime(transcript) {
+        // let transcript = [];
+        // transcript = await databaseFunctions.getTrim();
+
+        const l = transcript.length;
+
+        let end_time = Date.parse(transcript[l - 1].startTime);
+        let start_time = Date.parse(transcript[0].startTime);
+        // console.log(start_time);
+        // console.log(Math.abs(end_time - start_time));
+        return Math.abs(end_time - start_time);
+    }
+
+
+    /**
+     * Return the possible reasons why user left the chat.
+     * @param text_transcript
+     * @returns {*[]}
+     */
+    checkReason(text_transcript, Q3_text, transcript, Q3_time) {
+        let dialogues = text_transcript.dialogue;
+        let l_texts = this.getDurationTexts(dialogues);
+        let l_time = this.getDurationTime(transcript.transcript_data);
+
+        let reasons = [];
+        if (this.userForceQuit(dialogues)) {
+            if (isPrivacyConcern(dialogues)) {
+                reasons.push("privacy");
+            }
+            if (isNoSolution(dialogues)) {
+                reasons.push("nosolution");
+            }
+            if (isHumanInteraction(dialogues)) {
+                reasons.push("humaninteraction");
+            }
+            if (isLengthyConvo(l_texts, Q3_text, l_time, Q3_time)) {
+                reasons.push("lengthyConvo")
+            }
+            if (reasons.length == 0) {
+                reasons.push("other");
+            }
+
+        }
+        return reasons;
+    }
 }
+
+export default new AnalyseTranscriptInteractor();
 
 

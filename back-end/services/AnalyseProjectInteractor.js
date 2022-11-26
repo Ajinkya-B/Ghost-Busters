6c1902f13156c821e4bb4476cf2819d5de5fd61a
khushiagl <khushi.agrawal@mail.utoronto.ca>
2022-11-25 20:54:18 -0500

    added isLengthyConvo

--- back-end/services/AnalyseProjectInteractor.js
+++ back-end/services/AnalyseProjectInteractor.js
@@ -35,6 +35,43 @@ export function avgDurationTime(transcripts) {
 }
 
 /**
+ * Returns the third quantile ( 75 percentile) of the durations of the conversations.
+ * @param transcripts
+ * @returns {*}
+ */
+export function thirdQuantileTime(transcripts){
+    let l = transcripts.length;
+    let Q3 = Math.floor(3*(l + 1)/4);
+    let durations = [];
+    for (let j = 0; j < l; j++) {
+        durations.push(getDurationTime(transcripts[j].transcript_data));
+    }
+    durations.sort(function (a, b){return a-b});
+    console.log(durations);
+    return durations[Q3 - 1];
+
+}
+
+/**
+ * Returns the third quantile ( 75 percentile) of the length of the conversations.
+ * @param text_transcripts
+ * @returns {*}
+ */
+export function thirdQuantileTexts(text_transcripts){
+    let l = text_transcripts.length;
+    let Q3 = Math.floor(3*(l + 1)/4);
+    let durations = [];
+    for (let j = 0; j < l; j++) {
+        durations.push(getDurationTexts(text_transcripts[j].dialogue));
+    }
+    durations.sort(function (a, b){return a-b});
+    console.log(durations);
+    return durations[Q3 - 1];
+
+}
+
+
+/**
  * Returns the total numbers of users that force quit chats or weren't provided with satisfatory solutions.
  * @param text_transcripts
  * @returns {number}
@@ -58,18 +95,23 @@ export function totalUsersForceQuit(text_transcripts) {
  * @param text_transcripts
  * @returns {{other: number, human_interaction: number, no_solution: number, privacy: number}}
  */
-export function checkReasons(text_transcripts) {
-    let l = text_transcripts.length;
+export function checkReasons(text_transcripts, transcripts) {
+    let l_text = text_transcripts.length;
+    let Q3_text = thirdQuantileTexts(text_transcripts);
+    let Q3_time = thirdQuantileTime(transcripts);
     let reasons = {
         "privacy": 0,
         "no_solution": 0,
         "human_interaction": 0,
+        "lengthy_convo": 0,
         "other": 0
     }
-    for (let j = 0; j < l; j++) {
+    for (let j = 0; j < l_text; j++) {
 
-        let transcript = text_transcripts[j];
-        let temp = checkReason(transcript);
+        let textTranscript = text_transcripts[j];
+        let transcript = transcripts[j];
+
+        let temp = checkReason(textTranscript, Q3_text, transcript, Q3_time);
         if (temp.includes("privacy")) {
             reasons.privacy += 1;
         }
@@ -79,12 +121,14 @@ export function checkReasons(text_transcripts) {
         if ( temp.includes("humaninteraction")){
             reasons.human_interaction += 1;
         }
+        if (temp.includes("lengthyConvo")){
+            reasons.lengthy_convo += 1;
+        }
         if ( temp.includes("other")){
             reasons.other += 1;
         }
     }
     return reasons;
 }
-
 
 

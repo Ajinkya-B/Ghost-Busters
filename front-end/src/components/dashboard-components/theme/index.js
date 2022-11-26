4f9ad80008c056afe01c8482d0edaef5a55dcacd
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-25 22:31:43 -0500

    deleted unused files

--- front-end/src/components/dashboard-components/theme/index.js
+++ front-end/src/components/dashboard-components/theme/index.js
@@ -2,13 +2,9 @@ import React from "react";
 import PropTypes from 'prop-types';
 import { useMemo } from 'react';
 // @mui
-import { CssBaseline } from '@mui/material';
 import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
 //
 import palette from './palette';
-import shadows from './shadows';
-import typography from './typography';
-import GlobalStyles from './globalStyles';
 import customShadows from './customShadows';
 import ComponentsOverride from './overrides';
 
@@ -23,8 +19,6 @@ export default function ThemeProvider({ children }) {
     () => ({
       palette,
       shape: { borderRadius: 6 },
-      //typography,
-      //shadows: shadows(),
       customShadows: customShadows(),
     }),
     []
@@ -36,8 +30,6 @@ export default function ThemeProvider({ children }) {
   return (
     <StyledEngineProvider injectFirst>
       <MUIThemeProvider theme={theme}>
-        {/*<CssBaseline />*/}
-        {/*<GlobalStyles />*/}
         {children}
       </MUIThemeProvider>
     </StyledEngineProvider>

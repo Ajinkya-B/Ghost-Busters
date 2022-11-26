510eaadfbe6b3d09a51796d15ce30853667341e7
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-11-26 00:53:38 -0500

    Revert "writing tests"
    This reverts commit 848646651f841d9fc4f8449d881ad7dd8e2f3c79.
    
    # Conflicts:
    #	back-end/yarn.lock

--- front-end/src/__test__/DashboardWelcome.test.js
+++ front-end/src/__test__/DashboardWelcome.test.js
@@ -6,12 +6,10 @@ import React from 'react';
 import "@testing-library/jest-dom";
 import {cleanup, render} from '@testing-library/react';
 import DashboardWelcomePage from "../pages/DashboardWelcome.js";
-import {act} from "react-test-renderer";
 
 it("navigates to Manage Projects page when you click button", () => {
     const root = document.createElement('div');
     document.body.appendChild(root);
- 
-});
-
+    // render
+})
 

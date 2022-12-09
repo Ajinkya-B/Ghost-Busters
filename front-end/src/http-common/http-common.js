98ce054c54a68b1f0e2c38e06cf21e2fe059b58b
chelsea <cchelsea.wang@mail.utoronto.ca>
2022-12-09 12:22:07 -0500

    deleted upload from device component

--- front-end/src/http-common/http-common.js
+++ front-end/src/http-common/http-common.js
@@ -4,7 +4,4 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL,
-    // headers: {
-    //     "Content-type": "application/json"
-    // }
 });
